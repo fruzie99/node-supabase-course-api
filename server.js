376e5845df4948f46bba2c31b8036f3f41ba3149
fruzie99 <vmadhurisha99@gmail.com>
2026-03-08 14:26:31 +0530

    logger and server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-s
+const express=require("express");
+const cors=require("cors");
+const logger=require("./middleware/logger");
+const CourseRoutes=require("./routes/CourseRoutes");
+
+require("dotenv").config();
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger);    
+app.use("/",courseRoutes);
+
+const PORT=process.env.PORT||5174
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+}); 
