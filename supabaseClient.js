cee38563e7a5164edbca542efa6ab7c7be280ae2
fruzie99 <vmadhurisha99@gmail.com>
2026-03-08 14:52:36 +0530

    checking postman

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const {createClient}=require("@supabase/supabase-js");
 
-const supabase= createClient(process.env.SUPABASE_URL,
-process.env.SUPABASE_KEY);
+const supabase= createClient(process.env.VITE_SUPABASE_URL,
+process.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
 
 module.exports=supabase;
 
